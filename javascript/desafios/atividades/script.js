4ad8a935bee45611de576b60a1d67b31dbd15dc6
Pedro <pedrowilker1516@gmail.com>
2022-10-20 15:58:28 -0300

    Atv

--- javascript/desafios/atividades/script.js
+++ javascript/desafios/atividades/script.js
@@ -1,3 +1,4 @@
+/* Atividade 1  */
 var notaAluno = prompt("Please enter your note ");
 
 if (notaAluno <= 5.9) {
@@ -6,4 +7,20 @@ if (notaAluno <= 5.9) {
   document.write("<h1>O aluno foi aprovado</h1>");
 } else if (notaAluno > 9.9) {
   document.write("<h1>Parabéns aluno!!!</h1>");
+}
+
+/* Atividade 2  */
+
+var idade = prompt("Digite sua idade");
+
+if (idade > 0 && idade <= 15) {
+  document.write("Criança");
+} else if (idade > 15 && idade <= 30) {
+  document.write("Jovem");
+} else if (idade > 30 && idade <= 60) {
+  document.write("Adulto");
+} else if (idade > 60) {
+  document.write("Idoso");
+} else {
+  document.write("Erro");
 }
